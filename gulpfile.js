3e0a90dba70d98750b8f82bb243b5b10c2afd9c5
unknown <ARL0427@MSP22MRA070216.daugherty.com>
2015-10-15 16:38:58 -0500

    Adds production flag to minify and uglify css and js

--- gulpfile.js
+++ gulpfile.js
@@ -6,9 +6,16 @@ var sourcemaps = require('gulp-sourcemaps');
 var sass = require('gulp-sass');
 var webpack = require("webpack");
 var WebpackDevServer = require("webpack-dev-server");
+var argv = require('yargs').argv;
+var gulpif = require('gulp-if');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
+var cssmin = require('gulp-cssmin');
+var del = require('del')
 
 
 var CONFIG = {
+  PROD_FLAG: 'prod',
   SASS: {
     INPUT: './src/styles/**/*.scss',
     OUTPUT: './web/styles',
@@ -26,6 +33,7 @@ var CONFIG = {
 }
 
 gulp.task('sass', function () {
+  del(['web/styles/*']);
   return gulp
     .src(CONFIG.SASS.INPUT)
     .pipe(sourcemaps.init())
@@ -36,25 +44,41 @@ gulp.task('sass', function () {
 
 
 gulp.task("webpack", function(callback) {
-    // run webpack
-    webpack({
-      entry: CONFIG.JS.INPUT,
-      output: {
-        path: CONFIG.JS.OUTPUT_DIR,
-        filename: CONFIG.JS.OUTPUT_FILE
-      },
-      devtool: 'source-map'
-    }, function(err, stats) {
-        if(err) throw new gutil.PluginError("webpack", err);
-        gutil.log("[webpack]", stats.toString({
-            // output options
-        }));
-        callback();
-    });
+  del(['web/js/*']);
+  // run webpack
+  webpack({
+    entry: CONFIG.JS.INPUT,
+    output: {
+      path: CONFIG.JS.OUTPUT_DIR,
+      filename: CONFIG.JS.OUTPUT_FILE
+    },
+    devtool: 'source-map'
+  }, function(err, stats) {
+      if(err) throw new gutil.PluginError("webpack", err);
+      gutil.log("[webpack]", stats.toString({
+          // output options
+      }));
+      callback();
+  });
 });
 
 
 gulp.task('default', ['sass','webpack'], function() {
-  gulp.watch(CONFIG.SASS.INPUT, ['sass']);
-  gulp.watch(CONFIG.JS.INPUT_GLOB, ['webpack']);
+  if (!argv[CONFIG.PROD_FLAG]) {
+    // setup up watches
+    gulp.watch(CONFIG.SASS.INPUT, ['sass']);
+    gulp.watch(CONFIG.JS.INPUT_GLOB, ['webpack']);
+  } else {
+    // uglify js
+    gulp
+      .src(CONFIG.JS.OUTPUT_DIR + CONFIG.JS.OUTPUT_FILE)
+      .pipe(rename({suffix: '.min'}))
+      .pipe(uglify())
+      .pipe(gulp.dest(CONFIG.JS.OUTPUT_DIR));
+    // minify css
+    gulp.src(CONFIG.SASS.OUTPUT + '/*.css')
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(CONFIG.SASS.OUTPUT));
+  }
 });
